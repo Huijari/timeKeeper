454728ce5121f4311f3af4b38805e8031219700a
santosmgbh <santosmgbh@gmail.com>
2017-06-10 12:39:31 -0300

    base controller

---
+++ src/main/java/br/com/avenuecode/grupo5/timeKeeper/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package br.com.avenuecode.grupo5.timeKeeper.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.avenuecode.grupo5.timeKeeper.entities.Usuario;
+
+@RestController
+public class UsuarioController {
+	
+	@RequestMapping(path = "/usuario")
+	public Usuario save(Usuario usuario){
+		return usuario;		
+	}
+	
+	@RequestMapping(path = "/usuario")
+	public Usuario update(@RequestParam(value="usuario") Usuario usuario){
+		return usuario;
+		
+		
+	}
+
+}
