ed06ccaa16ab0a3e17e07d4334f44acb778c854c
lucascruz96 <lucascruz96@outlook.com>
2017-06-10 11:45:50 -0300

    Alteração do escopo

---
+++ src/main/java/br/com/avenuecode/grupo5/timeKeeper/TimeKeeperApplication.java
@@ -0,0 +1,12 @@
+package br.com.avenuecode.grupo5.timeKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeKeeperApplication.class, args);
+	}
+}
