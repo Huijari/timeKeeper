a83927059e85a88971628db13595fd69f1cf6aa9
lucascruz96 <lucascruz96@outlook.com>
2017-06-10 14:01:43 -0300

    Get usuario

--- src/main/java/br/com/avenuecode/grupo5/timeKeeper/controller/UsuarioController.java
+++ src/main/java/br/com/avenuecode/grupo5/timeKeeper/controller/UsuarioController.java
@@ -1,10 +1,12 @@
 package br.com.avenuecode.grupo5.timeKeeper.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.avenuecode.grupo5.timeKeeper.entities.Usuario;
+
+import javax.xml.ws.Response;
 
 @RestController
 public class UsuarioController {
@@ -15,10 +17,18 @@ public class UsuarioController {
 	}
 	
 	@RequestMapping(path = "/usuario")
-	public Usuario update(@RequestParam(value="usuario") Usuario usuario){
+	public Usuario update(@RequestParam(value="usuario") Usuario usuario) {
 		return usuario;
-		
-		
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Usuario> listar(@PathVariable("id") Integer id){
+		Usuario usuario = usuarios.get(id);
+
+		if(usuario == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		else{
+			return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+		}
 }
