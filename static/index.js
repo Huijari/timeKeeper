fe06c3d5bc61755cf551e6d4695c7f252720a56f
Alexandre Cesar <acmorcego@gmail.com>
2017-06-10 18:18:13 -0300

    Implement home page

--- static/index.js
+++ static/index.js
@@ -2,23 +2,49 @@ import angular from 'angular'
 
 const app = angular.module('app', []);
 
-const base = 'http://localhost:8080/'
+const base = 'http://localhost:8080'
 
 app.controller('main', mainController)
 
-function mainController() {
+function mainController($interval, $http) {
   const vm = this
 
   vm.usuario = null
+  vm.data = new Date()
 
   vm.login = name => {
-    vm.usuario = {
-    }
+    $http.post(`${base}/usuario/login`, {
+      login: name
+    }).then(res => res.data)
+      .then(usuario => vm.usuario = adjustDates(usuario))
   }
 
   vm.logout = () => {
     vm.usuario = null
   }
 
+  vm.ponto = () => {
+    const pontoPromise = $http.post(`${base}/usuario/${vm.usuario.id}/ponto`)
+    pontoPromise.then(() =>
+      $http.get(`${base}/usuario/${vm.usuario.id}`)
+        .then(res => res.data)
+        .then(usuario => vm.usuario = adjustDates(usuario))
+    )
+  }
+
+  $interval(() => vm.data = new Date(), 500)
+
+  vm.login('acesar')
+
   return vm
+}
+
+function adjustDates(usuario) {
+  usuario.pontos = usuario.pontos.map(ponto => {
+    ponto.entrada = new Date(ponto.entrada)
+    if (ponto.saida !== null)
+      ponto.saida = new Date(ponto.saida)
+    return ponto
+  }).reverse()
+  return usuario
 }
