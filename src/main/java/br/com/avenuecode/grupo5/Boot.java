854fe55c85f0bc5911d71384767b8db8da83866d
lucascruz96 <lucascruz96@outlook.com>
2017-06-10 10:45:04 -0300

    Estrutura do projeto

---
+++ src/main/java/br/com/avenuecode/grupo5/Boot.java
@@ -0,0 +1,14 @@
+package br.com.avenuecode.grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(Boot.class, args);
+   }
+}
